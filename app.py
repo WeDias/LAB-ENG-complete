60172f36f8e3e9715f4f821f794ba1a20d3dd59d
WesVisiona <wesley.dias@visionaespacial.com.br>
2021-08-17 09:24:43 -0300

    Adição da base do projeto

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask
+
+
+app = Flask(__name__)
+app.config.from_object('config')
+
+
+@app.route('/')
+def index():
+    return "teste"
+
+
+if __name__ == '__main__':
+    app.run()
