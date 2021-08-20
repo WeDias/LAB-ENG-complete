37eb4979c38f5512b3be8e4bc8adf0bcdccd6612
WesVisiona <wesley.dias@visionaespacial.com.br>
2021-08-20 08:26:14 -0300

    Atualização srcript para configurar o ambiente automaticamente

--- execute.sh
+++ execute.sh
@@ -1,2 +1,12 @@
-. venv/bin/activate
+# Configuração da venv
+ACTIVATE_FILE=venv/bin/activate
+
+if [ ! -f "$ACTIVATE_FILE" ]; then
+    python3 -m venv venv
+    . "$ACTIVATE_FILE"
+    pip install -r requirements.txt
+fi
+
+# Execução da aplicação
+. "$ACTIVATE_FILE"
 gunicorn --bind 0.0.0.0:5000 app:app
