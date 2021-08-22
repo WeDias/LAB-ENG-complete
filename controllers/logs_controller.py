1dde996632b07c20016b25aa5ecc2ab098a66a8c
WesVisiona <wesley.dias@visionaespacial.com.br>
2021-08-22 00:51:54 -0300

    Adição order_by desc nos logs

--- controllers/logs_controller.py
+++ controllers/logs_controller.py
@@ -8,5 +8,6 @@ logs_controller = Blueprint('logs_controller', __name__)
 @logs_controller.route('/logs', methods=['GET'])
 def get_calculadora():
     data = base.session.query(LogModel, OperationTypeModel) \
-        .join(OperationTypeModel, OperationTypeModel.opt_id == LogModel.log_opt_id).all()
+        .join(OperationTypeModel, OperationTypeModel.opt_id == LogModel.log_opt_id) \
+        .order_by(LogModel.log_id.desc()).all()
     return render_template('logs_template.html', data=data)
